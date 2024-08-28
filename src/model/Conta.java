05586b201450b0fa2325e6ccd7aeffd2c834bb68
Lucas Rocha <lr359508@gmail.com>
2024-08-28 06:47:09 -0300

    feat: modelo de conta finalizado

--- src/model/Conta.java
+++ src/model/Conta.java
@@ -5,31 +5,71 @@ public class Conta extends GenericModel {
     private String cpf;
     private double renda_mensal;
     private double saldo;
+    private double divida;
     private String tipo_conta;
 
-    public Conta(String nome, String cpf, double renda_mensal, double saldo, String tipo_conta) {
+    public Conta(String nome, String cpf, double renda_mensal, double saldo, double divida, String tipo_conta) {
         this.nome = nome;
         this.cpf = cpf;
         this.renda_mensal = renda_mensal;
         this.saldo = saldo;
+        this.divida = divida;
         this.tipo_conta = tipo_conta;
     }
 
-    public Conta(Integer id, String nome, String cpf, double renda_mensal, double saldo, String tipo_conta) {
-        this(String nome, String cpf, double renda_mensal, double saldo, String tipo_conta)
+    public Conta(Integer id, String nome, String cpf, double renda_mensal, double saldo, double divida, String tipo_conta) {
+        this(nome, cpf, renda_mensal, saldo, divida, tipo_conta);
         super.setId(id);
     }
 
-    // Fazer métodos especiais para alguns atributos
-    public String getDescricao() {
-        return descricao;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
-    // Ajeitar
+    public double getRenda_mensal() {
+        return renda_mensal;
+    }
+
+    public void setRenda_mensal(double renda_mensal) {
+        this.renda_mensal = renda_mensal;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTipo_conta() {
+        return tipo_conta;
+    }
+
+    public void setTipo_conta(String tipo_conta) {
+        this.tipo_conta = tipo_conta;
+    }
+
+    public double getDivida() {
+        return divida;
+    }
+
+    public void setDivida(double divida) {
+        this.divida = divida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     @Override
     public String toString() {
         return "Conta {" +
@@ -37,6 +77,7 @@ public class Conta extends GenericModel {
                 "cpf='" + cpf + "\'" +
                 "renda_mensal='" + renda_mensal + "\'" +
                 "saldo='" + saldo + "\'" +
+                "divida='" + divida + "\'" +
                 "tipo_conta='" + tipo_conta + "\'" +
                 '}';
     }
