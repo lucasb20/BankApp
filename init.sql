b1a9577707fced9bc323804a7fa12a5228ed1672
Lucas Rocha <lr359508@gmail.com>
2024-09-02 07:44:39 -0300

    feat: tabelas de reserva criadas

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pessoa (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(45),
+    nome VARCHAR(255),
     cpf CHAR(11) UNIQUE
 );
 
@@ -24,37 +24,18 @@ CREATE TABLE tipo_conta (
     descricao VARCHAR(45)
 );
 
-CREATE TABLE movimentacao_conta (
+CREATE TABLE reserva (
     id SERIAL PRIMARY KEY,
-    valor DOUBLE PRECISION,
-    dt_criacao Timestamp,
-    tipo_movimentacao_saida BIT,
-    conta_id INTEGER REFERENCES conta(id)
-);
-
-CREATE TABLE cartao_de_credito (
-    id SERIAL PRIMARY KEY,
-    dt_fechamento Timestamp,
-    dt_limite Timestamp,
-    limite_credito DOUBLE PRECISION,
+    saldo DOUBLE PRECISION,
+    taxa DOUBLE PRECISION,
+    reservacol VARCHAR(45),
     conta_id INTEGER REFERENCES conta(id),
-    numero_cartao CHAR(16),
-    cvc smallint,
-    tipo_cartao_fisico BIT
 );
 
-CREATE TABLE movimentacao_credito (
+CREATE TABLE movimentacao_reserva (
     id SERIAL PRIMARY KEY,
-    dt_criacao Timestamp,
+    dt_movimentacao Timestamp,
     valor DOUBLE PRECISION,
-    quantidade_de_parcelas smallint,
-    cartao_de_credito_id INTEGER REFERENCES cartao_de_credito(id)
-);
-
-CREATE TABLE fatura (
-    id SERIAL PRIMARY KEY,
-    valor_total DOUBLE PRECISION,
-    dt_criacao Timestamp,
-    cartao_de_credito_id INTEGER REFERENCES cartao_de_credito(id),
-    is_paid bit
+    tipo_movimentacao VARCHAR(45),
+    reserva_id INTEGER REFERENCES reserva(id),
 );
