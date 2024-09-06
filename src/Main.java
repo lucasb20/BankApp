e535c3fd74a0b3b7bd87818f0bc7e48c53551947
Lucas Rocha <lr359508@gmail.com>
2024-09-06 15:42:13 -0300

    fix: correção de testes

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,28 @@ import model.Pessoa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
         ServicoPessoa servicoPessoa = new ServicoPessoa();
         Pessoa pessoa1 = new Pessoa("Lucas Rcha", "12345678910");
         Pessoa pessoa2 = new Pessoa("Jacinto Ben", "12345678911");
 
         servicoPessoa.insertPessoa(pessoa1);
         servicoPessoa.insertPessoa(pessoa2);
-        System.out.printf(servicoPessoa.selectPessoa(1).toString());
-        System.out.printf(servicoPessoa.selectPessoa(2).toString());
+        
+        pessoa1 = servicoPessoa.selectPessoa(1);
+        pessoa2 = servicoPessoa.selectPessoa(2);
+
+        System.out.printf(pessoa1.toString() + '\n');
+        System.out.printf(pessoa2.toString() + '\n');
 
         pessoa1.setNome("Lucas Rocha");
         servicoPessoa.updatePessoa(pessoa1);
         servicoPessoa.deletePessoa(2);
-        System.out.printf(servicoPessoa.selectPessoa(1).toString());
-        System.out.printf(servicoPessoa.selectPessoa(2).toString());
+        System.out.printf(servicoPessoa.selectPessoa(1).toString() + '\n');
+
+        try {
+            System.out.printf(servicoPessoa.selectPessoa(2).toString() + '\n');
+        } catch (NullPointerException e) {
+            System.out.println("Jacinto Ben deletado com sucesso.");
+        }
     }
 }
