3d41dd737d02fc8792b47ccbf9e2580907449156
Lucas Rocha <lr359508@gmail.com>
2024-09-08 15:15:13 -0300

    feat: inserções de movimentação reserva ajustadas

--- src/Main.java
+++ src/Main.java
@@ -105,9 +105,9 @@ public class Main {
         // MovimentacaoReserva
         service.ServicoMovimentacaoReserva servicoMovimentacaoReserva = new service.ServicoMovimentacaoReserva();
 
-        servicoMovimentacaoReserva.insertMovimentacaoReserva(1000, "Entrada", "2021-10-10", 1);
-        servicoMovimentacaoReserva.insertMovimentacaoReserva(200, "saída", "2021-10-20", 1);
-        servicoMovimentacaoReserva.insertMovimentacaoReserva(300, "entrada", "2021-10-21", 1);
+        servicoMovimentacaoReserva.insertMovimentacaoReserva(1000, "Entrada", "2021-10-10 23:00:00", 1);
+        servicoMovimentacaoReserva.insertMovimentacaoReserva(200, "saída", "2021-10-20 14:30:00", 1);
+        servicoMovimentacaoReserva.insertMovimentacaoReserva(300, "entrada", "2021-10-21 08:30:00", 1);
 
         List<model.MovimentacaoReserva> movimentacaoReservas = servicoMovimentacaoReserva.selectAllMovimentacaoReserva(1);
 
